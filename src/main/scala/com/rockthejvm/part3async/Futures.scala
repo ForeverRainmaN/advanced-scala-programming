10f5ef8d343692d0e492afc8a882896d227181a2
Курчин Иоанн Вячеславович <lvrainman@gmail.com>
2025-01-05 02:32:30 +0300

    lesson-21-futures-part2

--- src/main/scala/com/rockthejvm/part3async/Futures.scala
+++ src/main/scala/com/rockthejvm/part3async/Futures.scala
@@ -2,7 +2,7 @@ package com.rockthejvm.part3async
 
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Random, Success, Try}
 
 object Futures {
 
@@ -28,6 +28,95 @@ object Futures {
     case Success(value) => println(s"I've completed with the meaning of life: $value")
     case Failure(exception) => println(s"My async computation failed: $exception")
   } // on SOME other thread
+
+  /*
+    Functional Composition
+   */
+
+  case class Profile(id: String, name: String) {
+    def sendMessage(anotherProfile: Profile, message: String) =
+      println(s"${this.name} sending message to ${anotherProfile.name}: $message")
+  }
+
+  object SocialNetwork {
+    // "database"
+    val names = Map(
+      "rtjvm.id.1-daniel" -> "Daniel",
+      "rtjvm.id.2-jane" -> "Jane",
+      "rtjvm.id.3-mark" -> "Mark",
+    )
+
+    val friends = Map(
+      "rtjvm.id.2-jane" -> "rtjvm.id.3-mark"
+    )
+
+    val rnd = new Random()
+
+    // "API
+    def fetchProfile(id: String): Future[Profile] = Future {
+      // fetch something from the database
+      Thread.sleep(rnd.nextInt(300)) // simulate the time delay
+      Profile(id, names(id))
+    }
+
+    def fetchBestFriend(profile: Profile): Future[Profile] = Future {
+      Thread.sleep(rnd.nextInt(400))
+      val bestFriendId = friends(profile.id)
+      Profile(bestFriendId, names(bestFriendId))
+    }
+  }
+
+  // problem: sending a message to my best friend
+
+  def sendMessageToBestFriend(accountId: String, message: String): Unit = {
+    val profileFuture = SocialNetwork.fetchProfile(accountId)
+    profileFuture.onComplete {
+      case Success(profile) =>
+        val friendProfileFuture = SocialNetwork.fetchBestFriend(profile)
+        friendProfileFuture.onComplete {
+          case Success(friendProfile) => profile.sendMessage(friendProfile, message)
+          case Failure(ex) => ex.printStackTrace()
+        }
+      // TODO steps 2 & 3
+      case Failure(ex) => ex.printStackTrace()
+    }
+  }
+
+  // onComplete is a hassle
+  // solution: functional composition
+
+  def sendMessageToBestFriend_v2(accountId: String, message: String): Unit = {
+    val profileFuture = SocialNetwork.fetchProfile(accountId)
+    val action: Future[Unit] = profileFuture.flatMap {
+      profile =>
+        SocialNetwork.fetchBestFriend(profile).map {
+          bestFriend => profile.sendMessage(bestFriend, message)
+        }
+    }
+  }
+
+  def sendMessageToBestFriend_v3(accountId: String, message: String): Unit = {
+    for {
+      profile <- SocialNetwork.fetchProfile(accountId)
+      bestFriend <- SocialNetwork.fetchBestFriend(profile)
+    } yield profile.sendMessage(bestFriend, message)
+  }
+
+  // fallbacks
+  val profileNoMatterWhat = SocialNetwork.fetchProfile("unknown id").recover {
+    case e: Throwable => Profile("rtjvm.id.0-dummy", "Forever Alone")
+  }
+
+  val aFetchedProfileNoMatterWhat: Future[Profile] = SocialNetwork.fetchProfile("unknown id").recoverWith {
+    case e: Throwable => SocialNetwork.fetchProfile("rtjvm.id.0-dummy")
+  }
+
+  val fallbackProfile: Future[Profile] = SocialNetwork.fetchProfile("unknown id").fallbackTo(SocialNetwork.fetchProfile("rtjvm.id.0-dummy"))
+
+  val janeProfileFuture = SocialNetwork.fetchProfile("rtjvm.id.2-jane")
+  val janeFuture: Future[String] = janeProfileFuture.map(profile => profile.name) // map transforms value contained inside, ASYNC
+  val janesBestFriend: Future[Profile] = janeProfileFuture.flatMap(profile => SocialNetwork.fetchBestFriend(profile))
+  val janesBestFriendFilter: Future[Profile] = janesBestFriend.filter(profile => profile.name.startsWith("Z"))
 
   def main(args: Array[String]): Unit = {
     println(futureInstantResult) // Inspect the value of the future right now
