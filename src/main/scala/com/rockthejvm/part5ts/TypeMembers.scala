413d3cdb658ac3804dfa030eff43de21de49c2f4
Курчин Иоанн Вячеславович <lvrainman@gmail.com>
2025-01-11 23:53:26 +0300

    lesson-39-type-members

---
+++ src/main/scala/com/rockthejvm/part5ts/TypeMembers.scala
@@ -0,0 +1,45 @@
+package com.rockthejvm.part5ts
+
+object TypeMembers {
+  class Animal
+
+  class Dog extends Animal
+
+  class Cat extends Animal
+
+  class AnimalCollection {
+    type AnimalType // abstract type member
+    type BoundedAnimal <: Animal // abstract type member with a type bound
+    type SuperBoundedAnimal >: Dog <: Animal
+    type AnimalAlias = Cat
+    type NestedOption = List[Option[Option[Int]]] // often used to alis complex/nested types
+  }
+
+  // using type members
+  val ac = new AnimalCollection
+  val anAnimal: ac.AnimalType = ???
+
+  // val cat: ac.BoundedAnimal = new Cat // BoundedAnimal might be dog
+  val aDog: ac.SuperBoundedAnimal = new Dog // ok, Dog <: SuperBoundedAnimal
+  val aCat: ac.AnimalAlias = new Cat // ok, Cat == AnimalAlias
+
+  // establish relationships between types
+  // alternative to generics
+  class LList[T] {
+    def add(element: T): LList[T] = ???
+  }
+
+  class MyList {
+    type T
+
+    def add(element: T): MyList = ???
+  }
+
+  // .type
+  type CatType = aCat.type
+  val newCat: CatType = aCat
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
